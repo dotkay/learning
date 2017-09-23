52cf8886a841eec193a90ce76cfd3052a7deef76
dotkay <kalyanvk@gmail.com>
2017-09-22 23:38:52 -0700

    added implementations of map and flatten

--- misc/perm.ml
+++ misc/perm.ml
@@ -1,9 +1,25 @@
 let rec insert x lst =
   match lst with
   | [] -> [[x]]
-  | h::t -> (x::lst) :: (List.map (fun el -> h::el) (insert x t));;
+  | h::t -> 
+    (x::lst) :: (List.map (fun el -> h::el) (insert x t));;
   
 let rec perm lst =
   match lst with
   | [] -> [lst]
-  | h::t -> List.flatten (List.map (insert h) (perm t));;
+  | h::t -> 
+    List.flatten (List.map (insert h) (perm t));;
+
+let flatn lst =
+  let rec aux acc l =
+    match l with
+    | [] -> acc
+    | h::t -> aux (acc @ h) t
+  in
+  aux [] lst;;
+
+let rec mapp f lst =
+  match lst with
+  | [] -> []
+  | h::t -> f h :: (mapp f t);;
+
