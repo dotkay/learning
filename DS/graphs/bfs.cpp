cc575b04040efe8b1cf8c994a9b0a836c4624134
dotkay <kalyanvk@gmail.com>
2018-09-20 14:53:40 +0100

    More intuitive example graph for BFS

--- DS/graphs/bfs.cpp
+++ DS/graphs/bfs.cpp
@@ -79,15 +79,29 @@ void Graph::BFS (int start) {
 
 int main () {
 
+  /*
   Graph g(4);
   g.add_edge(0, 1);
   g.add_edge(0, 2);
   g.add_edge(1, 2);
-  g.add_edge(2, 3);
   g.add_edge(2, 0);
+  g.add_edge(2, 3);
   g.add_edge(3, 3);
 
   g.BFS(2);
+  */
 
+  Graph g(6);
+  g.add_edge(0, 1);
+  g.add_edge(0, 2);
+  g.add_edge(1, 3);
+  g.add_edge(1, 4);
+  g.add_edge(2, 4);
+  g.add_edge(3, 4);
+  g.add_edge(3, 5);
+  g.add_edge(4, 5);
+
+  g.BFS(0);
+  
   return 0;
 }
