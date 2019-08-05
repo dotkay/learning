8c20a2d8615c281eaf07bc8c78390a65ea620e62
dotkay <kalyanvk@gmail.com>
2019-08-05 03:42:55 +0100

    singly linked list using one head pointer and iterative print function with insertion at head or at tail

---
+++ data_structures/linked_lists/singly_linked_list.cpp
@@ -0,0 +1,103 @@
+// linked_list.cpp
+
+#include <iostream>
+
+class Node 
+{
+  private:
+    int data;
+    Node* next;
+    Node(int item);
+  friend class List;
+};
+
+Node::Node(int item)
+{
+  this->data = item;
+  this->next = NULL;
+}
+
+class List
+{
+  private:
+    Node* head;
+    int length;
+  public:
+    List();
+    void insert_item(int item);
+    void tail_insert(int item);
+    int get_size();
+    Node* get_head();
+    void print_list(Node* head);
+};
+
+List::List()
+{
+  this->head = NULL;
+  this->length = 0;
+}
+
+// inserts item to the head of the list
+void List::insert_item(int item)
+{
+  if (head == NULL)
+    head = new Node(item);
+  else 
+  {
+    Node* tmp = new Node(item);
+    tmp->next = head;
+    head = tmp;
+    length++;
+  }
+}
+
+// inserts item to the tail of the list
+void List::tail_insert(int item)
+{
+  Node* tmp = head;
+  while (tmp->next != NULL)
+  {
+    tmp = tmp->next;
+  }  
+  Node* new_node = new Node(item);
+  tmp->next = new_node;
+}
+
+// get the length of the list
+int List::get_size()
+{
+  return (this->length);
+}
+
+// fetch the head pointer
+Node* List::get_head()
+{
+  return (this->head);
+}
+
+// prints the list
+void List::print_list(Node* head)
+{
+  Node* tmp = head;
+  while (tmp != NULL)
+  {
+    std::cout << tmp->data << " -> ";
+    tmp = tmp->next;
+  }
+  std::cout << "NULL" << std::endl;
+  free(tmp);
+}
+
+int main()
+{
+  List* l = new List();
+  l->insert_item(1);
+  l->insert_item(2);
+  l->insert_item(3);
+  l->insert_item(4);
+  l->print_list(l->get_head());
+  l->tail_insert(5);
+  l->print_list(l->get_head());
+
+  return 0;
+}
